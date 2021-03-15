87d2e9584300cc5977d36ce39fbdd33365578407
Hiroaki Date <mmst3l4@gmail.com>
2021-03-15 23:05:40 +0900

    [from now] 2021/03/15 23:05:40
    diff --git a/src/App.js b/src/App.js
    index 9efff94..2655afb 100644
    --- a/src/App.js
    +++ b/src/App.js
    @@ -1,22 +1,15 @@
     import React, { useState } from 'react';
    
     const App = props => {
    -
    -  const [name, setName] = useState(props.name);
    -  const [price, setPrice] = useState(props.price);
    -
    -  const reset = () => {
    -    setPrice(props.price);
    -    setName(props.name);
    -  };
    +  const [state, setState] = useState(props);
    
       return (
         <React.Fragment>
    -      <p>現在の、{name}は{price}です</p>
    -      <button onClick={() => setPrice(price + 1)}>+1</button>
    -      <button onClick={() => setPrice(price - 1)}>-1</button>
    -      <button onClick={reset}>reset</button>
    -      <input value={name} onChange={e => setName(e.target.value)} />
    +      <p>現在の、{state.name}は{state.price}です</p>
    +      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
    +      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
    +      <button onClick={() => setState(props)}>reset</button>
    +      <input value={state.name} onChange={e => setState({...state, name: e.target.value})} />
         </React.Fragment>
       );
     };

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
 
 const App = props => {
-
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  const [state, setState] = useState(props);
   
   return (
     <React.Fragment>
-      <p>現在の、{name}は{price}です</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <p>現在の、{state.name}は{state.price}です</p>
+      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>reset</button>
+      <input value={state.name} onChange={e => setState({...state, name: e.target.value})} />
     </React.Fragment>
   );
 };
