df01a6c77866f2977568fbf43e2e505d906254b7
Hiroaki Date <mmst3l4@gmail.com>
2021-03-18 20:55:44 +0900

    不要な記述を削除

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = props => {
-  const [state, setState] = useState(props);
-  
   return (
     <React.Fragment>
-      <p>現在の、{state.name}は{state.price}です</p>
-      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
-      <button onClick={() => setState(props)}>reset</button>
-      <input value={state.name} onChange={e => setState({...state, name: e.target.value})} />
+      hoge
     </React.Fragment>
   );
 };
