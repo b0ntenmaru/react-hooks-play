82c8cd81f71490a3e7aecf5eb197968f50729524
Hiroaki Date <mmst3l4@gmail.com>
2021-03-21 17:36:55 +0900

    [from now] 2021/03/21 17:36:55
    diff --git a/src/components/EventForm.js b/src/components/EventForm.js
    new file mode 100644
    index 0000000..731a1eb
    --- /dev/null
    +++ b/src/components/EventForm.js
    @@ -0,0 +1,59 @@
    +import React, { useState } from 'react';
    +
    +
    +const EventForm = ({dispatch, state}) => {
    +
    +  const [title, setTitle] = useState('');
    +  const [body, setBody] = useState('');
    +
    +  const unCreatable = title === '' || body === '';
    +
    +  const unDeletable = state.length === 0;
    +
    +  const addEvent = e => {
    +    e.preventDefault();
    +    dispatch({type: 'CREATE_EVENT', title, body});
    +
    +    setTitle('');
    +    setBody('');
    +  };
    +
    +  const deleteAllEvents = (e) => {
    +    e.preventDefault();
    +    const result = window.confirm('全てのイベントを削除して良いですか？')
    +
    +    if (result) dispatch({type: 'DELETE_ALL_EVENT'});
    +  }
    +
    +  return (
    +    <React.Fragment>
    +    <h4>イベント作成フォーム</h4>
    +      <form>
    +        <div className="form-group">
    +          <label htmlFor="formEventTitle">タイトル</label>
    +          <input
    +            className="form-control"
    +            id="formEventTitle"
    +            value={title}
    +            onChange={e => setTitle(e.target.value)}
    +          />
    +        </div>
    +
    +        <div className="form-group">
    +          <label htmlFor="formEventBody">ボディ</label>
    +          <textarea
    +            className="form-control"
    +            id="formEventBody"
    +            value={body}
    +            onChange={e => setBody(e.target.value)}
    +          />
    +        </div>
    +
    +        <button disabled={unCreatable} className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
    +        <button disabled={unDeletable} className="btn btn-danger" onClick={deleteAllEvents}>全てのイベントを削除する</button>
    +      </form>
    +    </React.Fragment>
    +  );
    +};
    +
    +export default EventForm;
    diff --git a/src/components/Events.js b/src/components/Events.js
    new file mode 100644
    index 0000000..d51e06c
    --- /dev/null
    +++ b/src/components/Events.js
    @@ -0,0 +1,25 @@
    +import React from 'react';
    +import Event from './Event';
    +
    +const Events = ({state, dispatch}) => {
    +  return (
    +    <React.Fragment>
    +    <h4>イベント一覧</h4>
    +    <table className="table table-hover">
    +      <thead>
    +        <tr>
    +          <th>ID</th>
    +          <th>タイトル</th>
    +          <th>ボディ</th>
    +          <th></th>
    +        </tr>
    +      </thead>
    +      <tbody>
    +        { state.map((event, index) => ( <Event key={index} event={event} dispatch={dispatch} />))}
    +      </tbody>
    +    </table>
    +    </React.Fragment>
    +  );
    +};
    +
    +export default Events;

---
+++ src/components/EventForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+ 
+const EventForm = ({dispatch, state}) => {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const unCreatable = title === '' || body === '';
+
+  const unDeletable = state.length === 0;
+
+  const addEvent = e => {
+    e.preventDefault(); 
+    dispatch({type: 'CREATE_EVENT', title, body});
+
+    setTitle('');
+    setBody('');
+  };
+
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm('全てのイベントを削除して良いですか？')
+
+    if (result) dispatch({type: 'DELETE_ALL_EVENT'});
+  }
+
+  return (
+    <React.Fragment>
+    <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input 
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={e => setTitle(e.target.value)} 
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディ</label>
+          <textarea 
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={e => setBody(e.target.value)}
+          />
+        </div>
+
+        <button disabled={unCreatable} className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
+        <button disabled={unDeletable} className="btn btn-danger" onClick={deleteAllEvents}>全てのイベントを削除する</button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default EventForm;
